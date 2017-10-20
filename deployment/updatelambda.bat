95b7ad89ba8abd5444254d80cac758153d4eee63
cyrus <cywong@vtc.edu.hk>
2017-10-20 16:35:59 +0800

    Initial Comments

---
+++ deployment/updatelambda.bat
@@ -0,0 +1,4 @@
+node C:\Users\developer\AppData\Roaming\npm\node_modules\grunt-cli\bin\grunt --gruntfile ..\Gruntfile.js lambda_package
+aws s3 cp ../dist/LexInterceptor_latest.zip s3://howwhofeelinvideopackage/LexInterceptor_latest.zip
+
+aws lambda update-function-code --function-name LexInterceptor-FacebookLexProxy-TUB2RGDX45MJ --s3-bucket howwhofeelinvideopackage --s3-key LexInterceptor_latest.zip
