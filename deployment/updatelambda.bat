a5a7ff0ab47a8ed61c1252275b4546bf0d2833f5
cyrus <cywong@vtc.edu.hk>
2017-11-01 23:55:10 +0800

    Add parameter to disable QRcode decode.

--- deployment/updatelambda.bat
+++ deployment/updatelambda.bat
@@ -1,4 +1,4 @@
 node C:\Users\developer\AppData\Roaming\npm\node_modules\grunt-cli\bin\grunt --gruntfile ..\Gruntfile.js lambda_package
 aws s3 cp ../dist/LexInterceptor_latest.zip s3://howwhofeelinvideopackage/LexInterceptor_latest.zip
 
-aws lambda update-function-code --function-name LexInterceptor-FacebookLexInterceptor-GX0ATCPP9EQ0 --s3-bucket howwhofeelinvideopackage --s3-key LexInterceptor_latest.zip
+aws lambda update-function-code --function-name LexInterceptor-FacebookLexInterceptorFunction-76LX7JP2MD0Y --s3-bucket howwhofeelinvideopackage --s3-key LexInterceptor_latest.zip
